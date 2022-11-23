1bfe56154412f32cc17c69e22e47a19c993f569d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 15:24:17 +0900

    add permission and authentication

--- youtube_api/settings.py
+++ youtube_api/settings.py
@@ -67,6 +67,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "youtube_api.wsgi.application"
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ]
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
