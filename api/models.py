78417d6a5b5ca18b6d0ce69188efaf89a10db2d1
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 17:11:52 +0900

    create User model

--- api/models.py
+++ api/models.py
@@ -24,3 +24,14 @@ class UserManager(BaseUserManager):
         return user
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
