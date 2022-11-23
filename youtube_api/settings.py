c7f022b99187ef5e8e66894f8cc3dd773e93eef8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 15:19:31 +0900

    add whitelist

--- youtube_api/settings.py
+++ youtube_api/settings.py
@@ -43,6 +43,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000"
+]
+
 ROOT_URLCONF = "youtube_api.urls"
 
 TEMPLATES = [
