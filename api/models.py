70ccafc2d426254d04969cf52d87d18af63e132f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 17:07:02 +0900

    create UserManager model

--- api/models.py
+++ api/models.py
@@ -4,3 +4,23 @@ from django.conf import settings
 import uuid
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('email is must')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.seve(using=self._db)
+
+        return user
+
+
