c0e8da13d97d20fb06264ce2758e78cb3a2531d7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 16:47:43 +0900

    import modules

--- youtube_api/urls.py
+++ youtube_api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("authen/", include("djoser.urls.jwt"))
+    path("authen/", include("djoser.urls.jwt")),
 ]
