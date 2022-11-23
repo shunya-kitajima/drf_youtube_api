ba046c669a11cfa551fe3cf091f9cea56f413aeb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-23 16:46:39 +0900

    add url

--- youtube_api/urls.py
+++ youtube_api/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include('api.urls'))
+    path("api/", include("api.urls")),
+    path("authen/", include("djoser.urls.jwt"))
 ]
