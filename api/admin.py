d69371287d06411e02ff9cdce81c10ab53eaad4f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 13:29:47 +0900

    import modules

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from .models import User, Video
 
-# Register your models here.
+admin.site.register(Video)
+
+
