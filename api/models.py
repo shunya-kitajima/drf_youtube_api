ab7742f90ad974c49bc3c246cc44433ef48feebf
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-24 13:42:21 +0900

    modify

--- api/models.py
+++ api/models.py
@@ -37,6 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     objects = UserManager()
     USERNAME_FIELD = 'email'
